5b63262ab5a09c40da3666ec75b885ae18f544b6
Ben Evans <bevans@newrelic.com>
2019-03-21 17:34:53 +0100

    Add one extra test

--- runtime/src/tests.rs
+++ runtime/src/tests.rs
@@ -30,3 +30,19 @@ fn test_read_simple_class() {
     assert_eq!("java/lang/Object", k.get_super_name());
     assert_eq!(2, k.get_methods().len());
 }
+
+#[test]
+fn check_simple_fields_methods() {
+    let bytes = match file_to_bytes(Path::new("../resources/test/octest/SimpleFieldsAndMethods.class")) {
+        Ok(buf) => buf,
+        _ => panic!("Error reading SimpleFieldsAndMethods"),
+    };
+    let mut parser = klass_parser::OtKlassParser::of(bytes, "octest/SimpleFieldsAndMethods.class".to_string());
+    parser.parse();
+    assert_eq!(23, parser.get_pool_size());
+    let k = parser.klass();
+    assert_eq!("octest/SimpleFieldsAndMethods", k.get_name());
+    assert_eq!("java/lang/Object", k.get_super_name());
+    assert_eq!(2, k.get_methods().len());
+    // assert_eq!(1, k.get_fields().len());
+}
